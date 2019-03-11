bc045bdea6043b69442aefbb9f04366d1ff300ea
Tanya102 <tanyachouhan98@gmail.com>
2019-03-11 08:08:07 +0530

    new web folder added

---
+++ Login Form/func.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p>JavaScript can change HTML attribute values.</p>
+
+<p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>
+
+<button onclick="document.getElementById('large').style.fontSize='40px'">Turn on the light</button>
+
+<p id='large'>This is it</p>
+
+<button onclick="document.getElementById('large')..style.fontSize='10px'">Turn off the light</button>
+
+</body>
+</html>
